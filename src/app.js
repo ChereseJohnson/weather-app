d0a355cdd5366ca25df4eb2429c599daa7ec7070
ChereseJohnson <cheresejohnson2010@gmail.com>
2022-02-06 20:45:32 -0500

    Added search engine

--- src/app.js
+++ src/app.js
@@ -34,8 +34,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "4141fa5fc237e61dbdda6ca492b87baf";
-let city = "Newark";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "4141fa5fc237e61dbdda6ca492b87baf";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Newark");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
